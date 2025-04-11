6a648dd55e79fcd6524a389e97775c4b93182904
Fazliddin <sarofiddinovfazliddin@gmail.com>
2025-04-11 20:23:26 +0500

    authentication

--- wway/settings/common.py
+++ wway/settings/common.py
@@ -156,6 +156,7 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
 }
 
